b3ac611724532ffcc2b4328b432686c6b0042207
Ina Vernikouskaya <ina.vernikouskaya@uni-ulm.de>
2020-03-29 16:12:53 +0200

    start to optimize dicom header generation/writing

--- DicomWriter.py
+++ DicomWriter.py
@@ -2,15 +2,18 @@
 # Produced by pydicom codify utility script
 from __future__ import unicode_literals  # Only for python2.7 and save_as unicode filename
 
+from datetime import datetime
 import os
 
 import pydicom
 from pydicom.dataset import Dataset
 from pydicom.sequence import Sequence
 
+
 class DicomWriter:
 
     def __init__(self):
+        self.ds = self.generateHeader()
         self.frameNumber = 0
         self.folderName = 'data'
 
@@ -21,22 +24,40 @@ class DicomWriter:
 
     def write(self, pixelData):
         # File meta info data elements
+        datetimeNow = datetime.now()
+        self.ds.InstanceCreationDate = datetimeNow.strftime("%Y%m%d")   #grabbing date
+        self.ds.InstanceCreationTime = datetimeNow.strftime("%H%M%S")  #grabbing time
+        self.ds.StudyDate = '0'              #primary angulation
+        self.ds.StudyTime = '0'              #secondary angulation
+        self.ds.KVP = "810.0"                #SPD
+        self.ds.DistanceSourceToDetector = "1200.0"  #SID
+        self.ds.StudyID = '60'               # vertical table pos
+        self.ds.SeriesNumber = "-760"        # longitudinal table pos
+        self.ds.AcquisitionNumber = "-110"   # lateral table pos
+        self.ds.PositionReferenceIndicator = '15'    #FD
+        self.ds.Rows = 1024                  #image width
+        self.ds.Columns = 1280               #image height
+        self.ds.PixelSpacing = [0.110726, 0.110726]  #pixel spacing
+        self.ds.PixelData = pixelData
+        self.frameNumber += 1
+        self.ds.save_as(self.folderName + '/' + str(self.frameNumber) + '.dcm', write_like_original=True)
+
+    def generateHeader(self):
         file_meta = Dataset()
         file_meta.FileMetaInformationGroupLength = 204
         file_meta.FileMetaInformationVersion = b'\x00\x01'
-        file_meta.MediaStorageSOPClassUID = '1.2.840.10008.5.1.4.1.1.2'
-        file_meta.MediaStorageSOPInstanceUID = '2.25.64478777239060462373300680991826309705'
-        file_meta.TransferSyntaxUID = '1.2.840.10008.1.2.1'
-        file_meta.ImplementationClassUID = '2.25.190146791043182537444806132342625375407'
-        file_meta.ImplementationVersionName = 'VTK_DICOM_0_8_6'
-
+        # file_meta.MediaStorageSOPClassUID = '1.2.840.10008.5.1.4.1.1.2'
+        # file_meta.MediaStorageSOPInstanceUID = '2.25.64478777239060462373300680991826309705'
+        # file_meta.TransferSyntaxUID = '1.2.840.10008.1.2.1'
+        # file_meta.ImplementationClassUID = '2.25.190146791043182537444806132342625375407'
+        # file_meta.ImplementationVersionName = 'VTK_DICOM_0_8_6'
         # Main data elements
         ds = Dataset()
         ds.ImageType = ['DERIVED', 'SECONDARY', 'OTHER']
         ds.InstanceCreationDate = '20180831'
         ds.InstanceCreationTime = '162416.733400'
-        ds.SOPClassUID = '1.2.840.10008.5.1.4.1.1.2'
-        ds.SOPInstanceUID = '2.25.64478777239060462373300680991826309705'
+        # ds.SOPClassUID = '1.2.840.10008.5.1.4.1.1.2'
+        # ds.SOPInstanceUID = '2.25.64478777239060462373300680991826309705'
         ds.StudyDate = '0'
         ds.StudyTime = '0'
         ds.AccessionNumber = ''
@@ -50,15 +71,15 @@ class DicomWriter:
         ds.SliceThickness = "1.0"
         ds.KVP = "810.0"
         ds.DistanceSourceToDetector = "1200.0"
-        ds.StudyInstanceUID = '2.25.305806315029320291821963682386599632641'
-        ds.SeriesInstanceUID = '2.25.235811267825957135853944735358538476705'
+        # ds.StudyInstanceUID = '2.25.305806315029320291821963682386599632641'
+        # ds.SeriesInstanceUID = '2.25.235811267825957135853944735358538476705'
         ds.StudyID = '60'
         ds.SeriesNumber = "-760"
         ds.AcquisitionNumber = "-110"
         ds.InstanceNumber = "1"
         ds.ImagePositionPatient = [0, 113.272698, 0]
         ds.ImageOrientationPatient = [1, 0, 0, 0, -1, 0]
-        ds.FrameOfReferenceUID = '2.25.248301001933451919654598313426997389484'
+        # ds.FrameOfReferenceUID = '2.25.248301001933451919654598313426997389484'
         ds.PositionReferenceIndicator = '15'
         ds.SliceLocation = "0.0"
         ds.SamplesPerPixel = 1
@@ -76,10 +97,7 @@ class DicomWriter:
         ds.LargestPixelValueInSeries = 255
         ds.RescaleIntercept = "0.0"
         ds.RescaleSlope = "1.0"
-        ds.PixelData = pixelData
-
         ds.file_meta = file_meta
         ds.is_implicit_VR = False
         ds.is_little_endian = True
-        self.frameNumber += 1
-        ds.save_as(self.folderName + '/' + str(self.frameNumber) + '.dcm', write_like_original=False)
+        return ds
