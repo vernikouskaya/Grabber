8d2dcdfd84ee5d13e2ad1c8c5ac1290a683143fc
Ina Vernikouskaya <ina.vernikouskaya@uni-ulm.de>
2020-04-14 13:27:43 +0200

    cropping of grabbed image at const 24 pixels from top and recognized shift in horizontal direction

--- Grabber.py
+++ Grabber.py
@@ -2,6 +2,7 @@ import time
 from datetime import datetime
 
 import cv2
+import numpy as np
 
 from DicomWriter import DicomWriter
 
@@ -35,20 +36,33 @@ class Grabber:
         print("Grab image from input #", self.input)
         timeStart = time.time()
         timeLast = timeStart
+        threshold = 50
+        x_shift = None
+        y_shift = None
+        x_start = 0
+        x_end = x_start + 255
+        #y_start = 169
+        y_start = 145 # calculating after cutting of upper 24 pixels
+        y_end = y_start + 255
+        y_cut = 24
         while (True):
             ret, read = self.capture.read()
             if not (ret):
                 print("frame not grabbed")
             else:
                 gray = cv2.cvtColor(read, cv2.COLOR_BGR2GRAY)
-                geometry = gray[168:423, 0:255]
+                indices = np.nonzero(gray > threshold)
+                x_shift = indices[1][0]
+                y_shift = indices[0][0]
+                gray_cut = gray[y_cut:1024, (x_start + x_shift):1280]
+                geometry = gray_cut[y_start:y_end, x_start:x_end]
                 #geometry = gray[168:210, 0:255] #primAngle
                 #cv2.imwrite('geometry.png', geometry)
                 #text = pytesseract.image_to_string(geometry)
                 #print(text)
-                writer.write(gray)
+                #writer.write(gray)
                 #cv2.imshow('color', read)
-                cv2.imshow('gray', gray)
+                cv2.imshow('gray', gray_cut)
                 cv2.imshow('geometry', geometry)
 
                 self.numFrames += 1
